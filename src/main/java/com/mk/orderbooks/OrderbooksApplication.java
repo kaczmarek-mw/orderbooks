ee42361fc957f1c192d078445c14fda1a3039576
michalkaczmarek <kaczmarek.mw@gmail.com>
2019-06-15 13:29:57 +0200

    Initial commit

---
+++ src/main/java/com/mk/orderbooks/OrderbooksApplication.java
@@ -0,0 +1,13 @@
+package com.mk.orderbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderbooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderbooksApplication.class, args);
+    }
+
+}
